6b185e2bebc2063e121a972bdf1c93dab7db129c
pickratt <151577410+pickratt@users.noreply.github.com>
2023-11-20 16:03:28 -0500

    Add files via upload

---
+++ BasicCalculatorDemo.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+
+public class BasicCalculatorDemo
+{
+    public static void main(String[] args)
+    {
+      BasicCalculator calculator = new BasicCalculator();
+      
+      calculator.setVisible(true);
+    }
+    public void add() //Allows the addition function
+    {
+	
+    }
+    public void subtract() //Allows the subtraction function
+    {
+	
+    }
+    public void squareRoot() //Allows the square root function
+    {
+
+
+    }
+
+
+    public void multiply () //Allows the multiplication function
+    {
+   
+    }
+   
+    public void divide() //Allows the division function
+    {
+       
+    }
+   
+    public void square() //Allows the square function
+    {
+
+
+    }
+    public void result() //Allows the equals function
+    {
+
+
+    }
+    public void clear() //Allows the calculator screen to be cleared
+    {
+
+
+    }
+    public void delete() //Allows a deletion from the screen
+    {
+	
+    }
+   
+}
