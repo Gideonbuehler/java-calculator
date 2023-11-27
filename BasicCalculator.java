e68c904c4bc761a5692bd164b2bfc4dd2a185d0d
ad1-123 <148586387+ad1-123@users.noreply.github.com>
2023-11-27 15:56:42 -0500

    Update BasicCalculator.java
    added unicode character for sqr to the button display

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -20,7 +20,7 @@ public class BasicCalculator extends JFrame
         // Initiates grid layout for JPanel
         JPanel buttonPanel = new JPanel(new GridLayout(5, 4)); // Sets the grid to 5 rows and 4 columns
         String[] buttonLabels = {
-                "sqr", "\u221a", "clear",
+                "x\u00b2", "\u221a", "clear",
                 "7", "8", "9", "/",
                 "4", "5", "6", "*",
                 "1", "2", "3", "-",
