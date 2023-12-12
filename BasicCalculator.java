dbbc4fc7e1f73ceb7f39617d97ca48c5c56d561d
pickratt <151577410+pickratt@users.noreply.github.com>
2023-12-12 13:23:13 -0500

    Update BasicCalculator.java
    deleted the pi again, the code seems to be a version before the other functions I put in.

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -192,12 +192,7 @@ public class BasicCalculator extends JFrame
 
                             x = 1/x;
                         }
-                        else if (isOperator('π')) {
-/* works ONLY if there is 
-a value before the symbol*/      nextChar();
-                                 x = x * Math.PI;
-//                                }
-                             }
+                        
                          else {
                             return x; // Returns x if no mult or division is found
                         }
